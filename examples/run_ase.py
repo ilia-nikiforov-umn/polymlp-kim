c62e6da38bf001aa955d52213e188835dd43316e
Atsuto Seko <sekocha@gmail.com>
2026-01-11 15:05:46 +0900

    Refactoring

--- examples/run_ase.py
+++ examples/run_ase.py
@@ -12,14 +12,14 @@ models = [
     "Polymlp_Seko_2022_AgAu__MO_020000020182_000",
 	"Polymlp_Seko_2022_AgAu__MO_020000120203_000",
 	"Polymlp_Seko_2022_AgAu__MO_020000220257_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000320269_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000420416_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000520441_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000620722_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000720725_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000820734_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020000910050_000",
-#	"Polymlp_Seko_2022_AgAu__MO_020001010051_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000320269_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000420416_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000520441_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000620722_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000720725_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000820734_000",
+	"Polymlp_Seko_2022_AgAu__MO_020000910050_000",
+	"Polymlp_Seko_2022_AgAu__MO_020001010051_000",
 ]
 
 axis = np.array(
