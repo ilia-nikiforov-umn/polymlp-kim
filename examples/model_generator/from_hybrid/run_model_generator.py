afec32e56b7f521ebf6b661d0ca5404a01f00b96
Atsuto Seko <sekocha@gmail.com>
2026-01-17 14:45:42 +0900

    Improve openmp support

---
+++ examples/model_generator/from_hybrid/run_model_generator.py
@@ -0,0 +1,12 @@
+"""Compute the cohesive energy, forces, and pressure of given POSCAR."""
+
+from pypolymlp.api.pypolymlp_utils import PypolymlpUtils
+
+polymlp = PypolymlpUtils()
+polymlp.generate_kim_model(
+    ["polymlp.lammps.1", "polymlp.lammps.2"],
+    performance_level=1,
+    project_id=1234567,
+    project_version=1,
+    model_driver="Polymlp__MD_000000123456_000",
+)
