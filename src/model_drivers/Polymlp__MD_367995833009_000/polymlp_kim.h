87b6fa3beb1dfa5024209b6dc84d03d83545ba94
Atsuto Seko <sekocha@gmail.com>
2026-02-25 09:06:22 +0900

    Ready to upload model driver

---
+++ src/model_drivers/Polymlp__MD_367995833009_000/polymlp_kim.h
@@ -0,0 +1,111 @@
+/* ----------------------------------------------------------------------
+   Contributing author: Atsuto Seko
+------------------------------------------------------------------------- */
+
+#ifndef POLYMLP_KIM
+#define POLYMLP_KIM
+
+#include "polymlp/polymlp_mlpcpp.h"
+#include "polymlp/polymlp_structs.h"
+#include "polymlp/polymlp_api.h"
+#include "polymlp/polymlp_functions_interface.h"
+
+#include "KIM_ModelDriverHeaders.hpp"
+#include "KIM_LogMacros.hpp"
+
+#include <cstring>
+
+#define DIMENSION 3
+
+typedef double VectorOfSizeDIM[DIMENSION];
+typedef double VectorOfSizeSix[6];
+
+
+class PolymlpKIM {
+
+    std::vector<PolymlpAPI> polymlp_array;
+    PolymlpAPI polymlp;
+    double cutoff_max;
+
+    int n_atoms_contrib;
+    vector1i map_contrib_to_full;
+    std::map<int, int> map_full_to_contrib;
+
+    // Compute properties using polymlp with polynomial invariants.
+    void compute_gtinv(
+        const KIM::ModelComputeArguments& model_compute_arguments,
+        int n_atoms,
+        const int * const atom_types,
+        const int * const contributing,
+        const VectorOfSizeDIM *& atom_coords,
+        double* energy,
+        double* atom_energy,
+        VectorOfSizeDIM *& forces,
+        double* virial,
+        VectorOfSizeSix *& particle_virial,
+        bool compute_process_dEdr);
+
+    void compute_sum_of_prod_anlmtp(
+        const KIM::ModelComputeArguments& model_compute_arguments,
+        int n_atoms, 
+        const int * const atom_types,
+        const int * const contributing,
+        const VectorOfSizeDIM *& atom_coords,
+        vector2dc& prod_sum_e, 
+        vector2dc& prod_sum_f);
+
+    // Compute properties using polymlp with pairwise features.
+    void compute_pair(
+        const KIM::ModelComputeArguments& model_compute_arguments,
+        int n_atoms,
+        const int * const atom_types,
+        const int * const contributing,
+        const VectorOfSizeDIM *& atom_coords,
+        double* energy,
+        double* atom_energy,
+        VectorOfSizeDIM *& forces,
+        double* virial,
+        VectorOfSizeSix *& particle_virial,
+        bool compute_process_dEdr);
+
+    void compute_sum_of_prod_antp(
+        const KIM::ModelComputeArguments& model_compute_arguments,
+        int n_atoms, 
+        const int * const atom_types,
+        const int * const contributing,
+        const VectorOfSizeDIM *& atom_coords,
+        vector2d& prod_sum_e, 
+        vector2d& prod_sum_f);
+
+    public:
+
+    PolymlpKIM(
+        const std::vector<std::string>& polymlp_files,
+        double energy_conv,
+        double length_conv,
+        double inv_length_conv);
+    ~PolymlpKIM();
+
+    void compute(
+        const KIM::ModelComputeArguments& model_compute_arguments,
+        int n_atoms, 
+        const int * const atom_types,
+        const int * const contributing,
+        const VectorOfSizeDIM *& atom_coords,
+        double* energy, 
+        double* atom_energy,
+        VectorOfSizeDIM *& forces,
+        double* virial,
+        VectorOfSizeSix *& particle_virial,
+        bool compute_process_dEdr);
+
+    double const * cutoff_ptr() const {
+        return &cutoff_max;
+    }
+
+    double prod_real(const dc val1, const dc val2){
+        return val1.real() * val2.real() - val1.imag() * val2.imag();
+    }
+};
+
+#endif
